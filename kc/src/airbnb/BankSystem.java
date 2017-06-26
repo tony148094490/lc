f844fbfcc98be6eb217d0a17a7dbcf840d85848f
Zhao <zhaowz@amazon.com>
2017-06-26 01:03:35 -0700

    Added stuff

--- kc/src/airbnb/BankSystem.java
+++ kc/src/airbnb/BankSystem.java
@@ -1,6 +1,46 @@
 package airbnb;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 //https://tinyurl.com/ycra756s
 //https://tinyurl.com/y9knfgok
+// Design a system that can deposit, withdraw and getRemaining given a timestamp
 public class BankSystem {
-
+	
+	// use ordered map to help with the date search later on.
+	// if treemap is not allowed, use linkedlist<Transaction> with binary search
+	Map<Long, TreeMap<Long, Integer>> map = new HashMap<>();
+	public void deposit(long accountId, int amount, long time) {
+		TreeMap<Long, Integer> accountBalance = map.getOrDefault(accountId, new TreeMap<>());
+		int currentBalance = 0;
+		if(accountBalance.size() > 0)
+		currentBalance = accountBalance.lastEntry().getValue();
+		
+		currentBalance += amount;
+		
+		accountBalance.put(time, currentBalance);
+		
+		map.put(accountId, accountBalance);
+	}
+	
+	public void withdraw(long accountId, int amount, long time) throws Exception {
+		if(!map.containsKey(accountId) || map.get(accountId).lastEntry().getValue() < amount) throw new Exception("Not enough balance for account: " + accountId);
+		map.get(accountId).put(time, map.get(accountId).lastEntry().getValue() - amount);
+	}
+	
+	// return the balance at start date and end date. Not between.
+	public int[] getRemain(long accountId, long start, long end, long time) throws Exception {
+		if(!map.containsKey(accountId)) throw new Exception("Acount " + accountId + " does not exist.");
+		int[] res = new int[2];
+		
+		if(map.get(accountId).floorEntry(start) != null)
+		res[0] = map.get(accountId).floorEntry(start).getValue();
+		
+		if(map.get(accountId).floorEntry(end) != null) 
+		res[1] = map.get(accountId).floorEntry(end).getValue();
+		
+		return res;
+	}
 }
--- kc/src/airbnb/BoggleGame.java
+++ kc/src/airbnb/BoggleGame.java
@@ -1,5 +1,114 @@
 package airbnb;
-
+/**
+ * Two variations:
+ * 1) char can be re-used
+ * 2) char cannot be reused
+ * 
+ * question: given a dict and a board, find the path that has largest number of word in a dict
+ */
+// i followed the solution below, a little hard to understand at first but eventually you'll get it
+// http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=214074
+// http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=191416
+// 小哥给的思路是double recursion，第一个function go through所有的position 如果这个position没有被visit过就call第二个function backtrack，
+// backtrack function里面如果目前的prefix不是一个词，就考虑上下左右继续backtrack，如果是一个词，就call第一个function，
+// 然后把这个词加到第一个function return的list里。为了加速搜索要用trie但trie写起来有点麻烦所以小哥就让写一个hashset里面有所有的prefix。时间有点久有点忘了但大概就是这样。。。
 public class BoggleGame {
-
+	
+	
+	
 }
+
+/**
+ * 
+Java: (This is the good one)
+Trie+dfs
+import java.util.*;
+
+class TNode{
+        TNode[] leaves;
+        boolean isword;
+        public TNode(){
+                this.leaves=new TNode[26];
+                this.isword=false;
+        }
+}
+
+public class Solution {
+        public static void main(String[] args){
+                String[] dict={"abs","abc","dd","bb"};
+                char[][] mat={{'a','b','c'},{'d','d','d'},{'b','b','d'}};
+                Solution sol=new Solution();
+                System.out.println(sol.findmaxPath(dict,mat));
+        }
+       
+        private int max=0;
+        private TNode root=new TNode();
+       
+        public int findmaxPath(String[] words, char[][] mat){
+                for(int i=0;i<words.length;i++){
+                        insertT(words[i]);
+                }
+                int range=mat.length*mat[0].length;
+                HashSet<Integer> set=new HashSet<Integer>();
+                for(int i=0;i<range;i++){
+                        pathfind(set,this.root,mat,i,0);
+                }
+                return this.max;
+        }
+       
+        public void pathfind(Set<Integer> set, TNode node, char[][] mat, int pos, int curcount){
+                if(node.isword){
+                        this.max=Math.max(max,curcount+1);
+                        node.isword=false;
+                        pathfind(set,this.root,mat,pos,curcount+1);
+                        node.isword=true;
+                }
+                int x=pos/mat[0].length;
+                int y=pos%mat[0].length;
+                if(node.leaves[mat[x][y]-'a']!=null){
+                        set.add(pos);
+                        for(Integer connect:connected(pos,mat)){
+                                if(!set.contains(connect)){
+                                        pathfind(set,node.leaves[mat[x][y]-'a'],mat,connect,curcount);
+                                }
+                        }
+                        set.remove(pos);
+                }
+                return;
+        }
+       
+        public List<Integer> connected(int pos, char[][] mat){
+                int m=mat.length;
+                int n=mat[0].length;
+                int x=pos/n;
+                int y=pos%n;
+                List<Integer> res=new ArrayList<Integer>();
+                for(int i=-1;i<=1;i+=2){
+                        if(x+i>=0&&x+i<m){
+                                res.add((x+i)*n+y);
+                        }
+                        if(y+i>=0&&y+i<n){
+                                res.add(x+y+i);
+                        }
+                }
+                return res;
+        }
+       
+        public void insertT(String s){
+                TNode cur=this.root;
+                for(int i=0;i<s.length();i++){
+                        if(cur.leaves[s.charAt(i)-'a']==null){
+                                 cur.leaves[s.charAt(i)-'a']=new TNode();
+                        }
+                        cur=cur.leaves[s.charAt(i)-'a'];
+                        if(i==s.length()-1){
+                                cur.isword=true;
+                        }
+                }
+        }
+}
+ * 
+ */
+
+
+
