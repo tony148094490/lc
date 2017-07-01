e2ee4870df7084884a20cfd931d7e9cbcc76e0c4
Zhao <zhaowz@amazon.com>
2017-07-01 01:21:20 -0700

    added

--- kc/src/airbnb/FavavateCities.java
+++ kc/src/airbnb/FavavateCities.java
@@ -1,7 +1,13 @@
 package airbnb;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
 
 /**
   Coding:给了一堆用户，每个用户有一堆想去的城市(用整数表示的)，这些城市是按照用户的喜好程度排序的，
@@ -13,17 +19,68 @@ import java.util.List;
     要求输出所有的城市，并且保持每个用户喜欢的顺序。其实就是topological sort.
  */
 public class FavavateCities {
-	public List<Integer> get(List<List<Integer>> list) {
+	public List<Integer> get(List<List<Integer>> lists) {
+		List<Integer> res = new ArrayList<>();
+		Map<Integer, Set<Integer>> map = new HashMap<>();
+		for(List<Integer> list : lists) {
+			for(int i = 1; i < list.size(); i++) {
+				int first = list.get(i-1);
+				int second = list.get(i);
+				if(!map.containsKey(first)) {
+					Set<Integer> set = new HashSet<>();
+					set.add(second);
+					map.put(first, set);
+				} else {
+					map.get(first).add(second);
+				}
+				
+				map.putIfAbsent(second, new HashSet<>());
+			}
+		}
+				
+		Set<Integer> overall = new HashSet<>();
+		Stack<Integer> stack = new Stack<>();
+		for(int x : map.keySet()) {
+			if(!overall.contains(x)) {
+				if(!dfs(map, x, new HashSet<>(), stack, overall)) return new ArrayList<>();
+			}
+		}
 		
+		while(!stack.isEmpty()) res.add(stack.pop());
+		
+		return res;
+	}
+	
+	private boolean dfs(Map<Integer, Set<Integer>> map, int cur, Set<Integer> visited, Stack<Integer> res, Set<Integer> overall) {
+		if(!map.containsKey(cur)) {
+			return true;
+		}
+		
+		if(visited.contains(cur)) return false;
+		
+		visited.add(cur);
+		
+		for(Integer nei : map.get(cur)) {
+			if(!dfs(map, nei, visited, res, overall)) return false;
+		}
+		
+		visited.remove(cur);
+		if(!overall.contains(cur)) {
+			overall.add(cur);
+			res.push(cur);
+		}
+		
+		return true;
 	}
 	
 	public static void main(String[] args) {
 		List<List<Integer>> res = new ArrayList<>();
-		int[] a = {10,2,5,20};
-		int[] b = {3,10,5,20};
-		int[] c = {4,3,8};
-		int[] d = {3,7,18,1};
+		List<Integer> a = Arrays.asList(10,2,5,20);
+		List<Integer> b = Arrays.asList(3,10,5,20);
+		List<Integer> c = Arrays.asList(4,3,8);
+		List<Integer> d = Arrays.asList(3,7,18,1);
+		res.add(a);res.add(b);res.add(c);res.add(d);
 		FavavateCities x = new FavavateCities();
-		
+		System.out.println(x.get(res));
 	}
 }
--- kc/src/airbnb/FileSystem.java
+++ kc/src/airbnb/FileSystem.java
@@ -1,4 +1,10 @@
 package airbnb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 //https://tinyurl.com/y7jwpjyo
 //https://tinyurl.com/ybfv5be8
 //https://tinyurl.com/y8zt49vs
@@ -6,14 +12,264 @@ package airbnb;
 //http://www.1point3acres.com/bbs/thread-220338-1-1.html
 /**
  * addValue(path, value), setValue(path, value), getValue, watch(path, callback)
- *
+ * 
+ * 1) 文件系统，每个文件夹或者文件有一个具体的数值，设计一个类，给定key可以update val可以insert val, 要求可以处理exception ?
+ * 
+ * 2) 大概意思是这样：每个目录对应一个value，比如addDir(/a,1),addDir(/a/b,1),但因为没有/c这个目录，
+ *  所以addDir(/c/b,3)就会报错，还可以修改目录的value，比如setDir(/a/b,2)。然后还可以watch(/a/b)，那么修改/a/b的时候就会调用一个Callback function
+ *  
+ * 3)  也很快做出来了。 Follow up之前也有人提过， 就是要设计一个用于报警的callback类。。这里就迷了。。 怎么写似乎面试官都不满意，
+ *  感觉communication又陷入了尴尬。 到了最后发现他好像要考一下OOD知识吧， 设计一个interface，然后再继承两个不同的callback类（虽然干得是同一件事）。
+ *  
+ * 4) 新题是一个OO Design，很简单，就是实现一个tree。 具体如下：
+	configuration tree：	
+	三个method：create(path, value), set_value(path, value), get_value(path)
+
+	让你实现一个长成这样的tree：
+
+    	root
+	  /     \
+	NA       EU
+  /    \
+ CA     US
+
+	其中root是没有name和value，剩下的每个点都有name和value。
+	create（path，value）：给你一个path，比如“NA/MX”，和value，比如“3”。那么你就在NA下面创建一个点叫MX，值是3。
+	set_value(path, value): 给你一个path，找到path的叶子，然后set value，如果叶子不存在，返回false；
+	get_value(path): 给你一个path，返回叶子的值，没有叶子的话返回NULL。
+	非常简单一个OOD，跟Trie很像。
+
+    5) 2. 感觉是做一个hashmap一样……有create(path,value), set_value(path,value), 和 get_value(path) 三个function
+	比如先：
+	create("eu","")
+	create("eu/france","")
+	这样就可以，因为你先建了eu了
+
+	但是如果没有create("eu/france","")，直接create("eu/france/paris","")，这样就不行。 反正这个很简单，就一个hashmap
+
+	followup是加一个watch(path, callback)方法，比如：
+	watch("eu",callback1)
+	watch("eu/france",callback2)
+
+	call set_value("eu/france","eu")时 会同时调用callback1和callback2 （可以理解吧，因为path里有eu也有eu/france）
+	我不知道function pointer怎么用在java里，所以跟面试官说改成了string
+
+
  */
 public class FileSystem {
-	public void updateWithKey(Key k, Value v) throws Exception {
-		
+	// needs to communicate and see if trie should be used 
+	private Dir root;
+
+	public FileSystem() {
+		root = new Dir(null);
 	}
 	
-	public void insertWithKey(Key k, Value v) throws Exception {
+	public void create(String path, String val) throws Exception {
+		//validate(path); 
+		if(path == null || path.isEmpty()) throw new Exception();
+		String[] orig = path.split("/");
+		List<String> list = new ArrayList<>();
+		for(String str : orig) {
+			if(str.isEmpty()) continue;
+			list.add(str);
+		}
 		
+		String[] dirs = new String[list.size()]; 
+		dirs = list.toArray(dirs);
+		
+		Dir cur = root;
+		for(int i = 0 ; i < dirs.length - 1; i++) {
+			String sub = dirs[i];
+			if(!cur.subDir.containsKey(sub)) throw new Exception();
+			cur = cur.subDir.get(sub);
+		}
+		String lastSub = dirs[dirs.length-1];
+		if(cur.subDir.containsKey(lastSub)) throw new Exception();
+		cur.subDir.put(lastSub, new Dir(val));
+	}
+	
+	public void setValue(String path, String value) throws Exception {
+		if(path == null || path.isEmpty()) throw new Exception();
+		String[] orig = path.split("/");
+		List<String> list = new ArrayList<>();
+		for(String str : orig) {
+			if(str.isEmpty()) continue;
+			list.add(str);
+		}
+		String[] dirs = new String[list.size()]; 
+		dirs = list.toArray(dirs);
+		
+		Dir cur = root;
+		for(int i = 0 ; i < dirs.length; i++) {
+			String sub = dirs[i];
+			if(!cur.subDir.containsKey(sub)) throw new Exception();
+			cur = cur.subDir.get(sub);
+			
+			// looks like callback is only invoked during setValue function, need to get clarity with interviewer
+			// also this line won't monitor the root dir, but can be changed easily to do so.
+			if(cur.callback != null) cur.callback.fireRules();
+		}
+		cur.value = value;
+	}
+	
+	public String getValue(String path) throws Exception {
+		if(path == null || path.isEmpty()) throw new Exception();
+		String[] orig = path.split("/");
+		List<String> list = new ArrayList<>();
+		for(String str : orig) {
+			if(str.isEmpty()) continue;
+			list.add(str);
+		}
+		String[] dirs = new String[list.size()]; 
+		dirs = list.toArray(dirs);
+		Dir cur = root;
+		for(int i = 0 ; i < dirs.length; i++) {
+			String sub = dirs[i];
+			if(!cur.subDir.containsKey(sub)) throw new Exception();
+			cur = cur.subDir.get(sub);
+		}
+		return cur.value;
+	}
+	
+	public void watch(String path, Callback callback) throws Exception {
+		if(path == null || path.isEmpty()) throw new Exception();
+		String[] orig = path.split("/");
+		List<String> list = new ArrayList<>();
+		for(String str : orig) {
+			if(str.isEmpty()) continue;
+			list.add(str);
+		}
+		String[] dirs = new String[list.size()]; 
+		dirs = list.toArray(dirs);
+		Dir cur = root;
+		for(int i = 0 ; i < dirs.length; i++) {
+			String sub = dirs[i];
+			if(!cur.subDir.containsKey(sub)) throw new Exception();
+			cur = cur.subDir.get(sub);
+		}
+		cur.callback = callback;
+	}
+	
+	public class Dir {
+		Map<String, Dir> subDir;
+		String value;
+		Callback callback;
+		
+		public Dir(String v) {
+			this.value = v;
+			subDir = new HashMap<>();
+		}
+	}
+	
+	// need to discuss Callback subclass design: multiple instances or multiple implementations
+	
+	public interface Callback {
+		public void fireRules();
+		public void notifyReceiver();
+	}
+	
+	public class CallbackPathImpl implements Callback {
+		String receiver;
+		String path;
+		public CallbackPathImpl(String receiver, String path) {
+			this.receiver = receiver;
+			this.path = path;
+		}
+		
+		public void fireRules() {
+			System.out.println(path + " changed value");
+		}
+		
+		public void notifyReceiver() {
+			// notify(receiver)...
+		}
+	}
+	
+	public class CallbackReceiverImpl implements Callback {
+		String receiver;
+		String path;
+		public CallbackReceiverImpl(String receiver, String path) {
+			this.receiver = receiver;
+			this.path = path;
+		}
+		
+		public void fireRules() {
+			System.out.println(path + " changed value " + receiver);
+		}
+		
+		public void notifyReceiver() {
+			// notify(receiver)...
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+		FileSystem fs = new FileSystem();
+		fs.create("/a", "1");
+		System.out.println("/a value is " + fs.getValue("/a"));
+		
+		try {
+			fs.create("/b/c", "2");
+		} catch (Exception e) {
+			System.out.println("error while processing creating path /b/c");
+		}
+		
+		fs.create("/a/b", "123");
+		
+		fs.setValue("/a/", "c");
+		System.out.println("/a value is " + fs.getValue("/a"));
+		
+		try {
+			fs.setValue("/b/c", "2");
+		} catch (Exception e) {
+			System.out.println("error while processing setting path /b/c");
+		}
+		
+		fs.setValue("/a/b", "456");
+		System.out.println("/a/b value is " + fs.getValue("/a/b"));
+
+		
+		fs.watch("/a", fs.new CallbackPathImpl("zhaowz", "/a"));
+		System.out.println("/a value is " + fs.getValue("/a"));
+		fs.setValue("/a/", "d");
+		System.out.println("/a value is " + fs.getValue("/a"));
+		
+		fs.watch("/a/b", fs.new CallbackReceiverImpl("zhaowz", "/a/b"));
+		System.out.println("/a/b value is " + fs.getValue("/a/b"));
+		fs.setValue("/a/b", "e");
+		System.out.println("/a/b value is " + fs.getValue("/a/b"));
 	}
 }
+/*
+ * Callback Example:
+Define an interface, and implement it in the class that will receive the callback.
+
+Have attention to the multi-threading in your case.
+
+Code example from http://cleancodedevelopment-qualityseal.blogspot.com.br/2012/10/understanding-callbacks-with-java.html
+
+interface CallBack {
+    void methodToCallBack();
+}
+
+class CallBackImpl implements CallBack {
+    public void methodToCallBack() {
+        System.out.println("I've been called back");
+    }
+}
+
+class Caller {
+
+    public void register(CallBack callback) {
+        callback.methodToCallBack();
+    }
+
+    public static void main(String[] args) {
+        Caller caller = new Caller();
+        CallBack callBack = new CallBackImpl();
+        caller.register(callBack);
+    }
+}
+
+*/ 
